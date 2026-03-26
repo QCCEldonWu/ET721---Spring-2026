46d53232d4830d6745cccdfe1c708c23554a915e
QCCEldonWu <eldon.wu19@student.qcc.cuny.edu>
2026-03-26 17:49:35 +0000

    Lab15

--- lab15/app.py
+++ lab15/app.py
@@ -35,14 +35,35 @@ def get_items():
     return jsonify(items)
 
 #READ SINGLE ITEM
-@app.route('/items/<item_id>', methods = ['GET'])
-def get_oneitem(item_id):
+@app.route('/items/<item_id>', methods = ['GET', 'PUT', 'DELETE'])
+def handle_item(item_id):
     item = items.get(item_id)
-    if not item:
-        # 404 = server is reachable but the item you asked for doesn't exist
-        return jsonify({'Error':"Item no found"}), 404
+
+    if request.method == 'GET':
+
+        if not item:
+            # 404 = server is reachable but the item you asked for doesn't exist
+            return jsonify({'Error':"Item no found"}), 404
     
-    return jsonify(item)
+        return jsonify(item)
+
+    # UPDATE
+    elif request.method == "PUT":
+        if not item:
+            return render_template('error.html', message = "ITEM NOT FOUND", item = item), 404
+        
+        data = request.get_json()
+        if not data:
+            return jsonify({'error' : 'Invalid input'}), 400
+        
+        items[item_id] = data
+        return render_template('update.html', item_id = item_id, item = data)
+
+    #DELETE
+    elif request.method == "DELETE":
+        deleted_item = items.pop(item_id)
+        return render_template('delete.html', item_id = item_id, deleted_item = deleted_item)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
